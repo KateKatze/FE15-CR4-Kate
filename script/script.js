6ccd05f91b0273d196ff703e4f4ded5c40d797cf
KateKatze <dr.katekogler@gmail.com>
2022-02-05 14:38:55 +0100

    started checking responsiveness

--- script/script.js
+++ script/script.js
@@ -16,10 +16,10 @@ for (let ind in arr) {
             <p class="card-text">${arr[ind].description}</p>
             <hr>
 
-            <div class="d-flex">
+            <div class="d-flex text-align justify-content-start">
                 <button class="priorlvl btn btn-md mb-1 bg-transparent"><i class="fa fa-hand-peace-o" style="font-size:20px;"></i></button>
-                <h6 class="mt-2">Priority level: </h6>
-                <p id="importn${ind}" class="card-text bg-success pt-1 ms-2">${arr[ind].importance}</p>
+                <h6 class="mt-3">Priority level: </h6>
+                <p id="importn${ind}" class="card-text rounded bg-success px-3 py-2 mt-1 ms-2 mb-2">${arr[ind].importance}</p>
             </div>
 
             <div class="d-flex">
@@ -63,6 +63,28 @@ for (let ind in arr) {
 }
 
 
+// Sort priority level / at least I tried!
+
+// 1. var arr (line 1) is already one array
+// 2. 
+
+// Next try:
+// var priorlvl = document.getElementsByClassName("priorlvl");
+// 
+// if (priorlvl[i].innerHTML > priorlvl[i + 1].innerHTML)
+//     priorlvl[i].innerHTML = `Priority level: ${count}`;
+// Bubble Sort
+
+
+
+// Priority level buttons:
+
+
+// First solution, buttons had an issue (count number was directly in the button, but colors didn´t change (only at the first loop)
+
+
+// for (let val of arr) {
+//     document.getElementById("result").innerHTML += `
 //     <div class="card col-lg-3 col-md-4 col-sm-11 m-1 shadow p-3 mb-5 bg-white rounded">
 //     <div class="card-header ">Task</div>
 //     <img src="${value.image}" class="card-img-top" alt="${value.taskName}">
@@ -74,50 +96,16 @@ for (let ind in arr) {
 //       <p class="card-text">${value.dueTime} <br></p>
 //       <hr>
 //       <p 
-//       <button id="importance" class="btn btn-md border pt-1 pb-1 mb-3 bg-success "><i class="fa fa-hand-peace-o"></i> Priority level:</button> <p class="impBut card-text">${value.importance}</p>
+//       <button id="importance" class="impBut btn btn-md border pt-1 pb-1 mb-3 bg-success "><i class="fa fa-hand-peace-o"></i> Priority level:</button> <p class="impBut card-text">${value.importance}</p>
 //       <p class="card-text float-end">
 //       <button class="btn btn-sm bg-danger text-light me-2"><i class="fa fa-trash-o me-1"></i>Delete</button><button class="btn btn-sm bg-success text-light"><i class="fa fa-check me-1"></i>Done</button>
 //       </p>
 //     </div>
 //   </div>
 
-
-
-
-
-
-// second loop makes button red
-
+// second loop makes button stay red
 // var count = 0;
 // var impBut = document.getElementsByClassName("impBut");
-
-// for (let i in arr) {
-//     impBut[i].onclick = function() {
-//         count++;
-//         if (count <= 5) {
-//             impBut[i].innerHTML = `${count}`;
-//         }
-
-//         switch (true) {
-//             case (count >= 0 && count <= 1):
-//                 impBut[i].classList.add("bg-success");
-//                 break;
-//             case (count >= 2 && count <= 3):
-//                 impBut[i].classList.add("bg-warning");
-//                 break;
-//             case (count >= 4 && count <= 5):
-//                 impBut[i].classList.add("bg-danger");
-//                 break;
-//             case (count > 5):
-//                 count = 0;
-//         }
-//     }
-// }
-
-
-// if (impBut[i].innerHTML > impBut[i + 1].innerHTML)
-//     impBut[i].innerHTML = `Priority level: ${count}`;
-// Bubble Sort
 
 // for (let i in arr) {
 //     impBut[i].onclick = function() {
@@ -136,47 +124,8 @@ for (let ind in arr) {
 //             case (count >= 4 && count <= 5):
 //                 impBut[i].classList.add("bg-danger");
 //                 break;
-//             default:
-//                 impBut[i].style = "white";
+//             case (count > 5):
+//                 count = 0;
 //         }
 //     }
-// }
-
-// Try #2 
-
-// for (let value in arr) {
-//     document.getElementById("result").innerHTML += `
-//     <div class="card cardDetails col-lg-3 col-md-4 col-sm-11 m-1 shadow p-3 mb-5 bg-white rounded">
-//     <img src="${arr[value].image}" class="card-img-top" alt="${arr[value].taskName}">
-//     <div class="card-body">
-//       <h5 class="card-title">${arr[value].taskName}</h5>
-//       <hr>
-//       <p class="card-text">${arr[value].description} <br></p>
-//       <hr>
-//       <p class="card-text">${arr[value].dueTime} <br></p>
-//       <hr>
-//       <button id="importance${value}" class="impBut btn btn-md border pt-1 pb-1 mb-3 bg-${color} ">Priority level: ${arr[value].importance}</button><br>
-//       <button class="btn btn-sm bg-danger text-light me-2">Delete</button><button class="btn btn-sm bg-success text-light">Done</button>
-//     </div>
-//   </div>
-//     `;
-// }
-
-// for (let index in data) {
-//     document.getElementsByClassName("impBut")[index].addEventListener("click", function() {
-//         document.getElementById(`importance${value}`).innerHTML++;
-//     })
-// }
-
-// Try #3
-
-// let countimport = document.getElementById("importance");
-// console.log(countimport);
-
-// let count = 0;
-// countimport.addEventListener("click", increment);
-
-// function increment() {
-//     count = count + 1;
-//     countimport.innerText = `Priority level: ${count}`;;
 // }
